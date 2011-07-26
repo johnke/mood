b22e5fb8c13ffd9214b40fc45906dd23c2facb34
John Kelly <johnke@gmail.com>
2011-07-26 22:06:44 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# What?
+
+Mood is a a life calendar that lets you keep track of your mood across the year and view it in one pretty view. It was inspired by an idea from [Brigada Creativa](http://brigadacreativa.bigcartel.com/product/life-calendar-how-was-your-day).
+
+## Installation
+
+1. Upload the files to a directory on your web server.
+2. Make sure the 'db' directory is writable.
+3. There is no step 3.
---
+++ index.php
@@ -0,0 +1,151 @@
+<?php
+try {
+    $db = new PDO('sqlite:db/mood.db');
+} catch (Exception $e) {
+    die($e);
+}
+
+if (isset($_POST['mood'])) {
+	$mood = $_POST['mood'];
+	$year = $_POST['year'];
+	$month = $_POST['month'];
+	$day = $_POST['day'];
+	$check_query = 'select mood from mood where year="'.$year.'" and month="'.$month.'" and day="'.$day.'"';
+	$mood_check = $db->query($check_query);
+	$num_rows = $mood_check->fetchColumn();
+	if ($num_rows > 0) {
+		$query = 'update mood set mood="'.$mood.'" where year = "'.$year.'" and month = "'.$month.'" and day = "'.$day.'"';
+	} else {
+		$query = 'insert into mood (mood, year, month, day) values ("'.$mood.'", "'.$year.'", "'.$month.'", "'.$day.'");';
+	}
+	$db->exec($query);
+}
+?><!doctype html>
+<html lang="en" class="no-js">
+<head>
+  <meta charset="utf-8">
+
+  <!--[if IE]><![endif]-->
+
+  <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+
+  <title>How was your day?</title>
+  <meta name="description" content="">
+  <meta name="author" content="John Kelly">
+
+  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+
+
+  <!-- CSS : implied media="all" -->
+  <link rel="stylesheet" href="/mood/css/font-face.css">
+  <link rel="stylesheet" href="/mood/css/style.css?v=1">
+
+  <link rel="stylesheet" media="handheld" href="/mood/css/handheld.css?v=1">
+
+</head>
+
+<!--[if lt IE 7 ]> <body class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <body class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <body class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <body class="ie9"> <![endif]-->
+<!--[if gt IE 9]>  <body>             <![endif]-->
+<!--[if !IE]><!--> <body>         <!--<![endif]-->
+
+
+
+  <div id="container">
+    <header>
+<h1>How was your day?</h1>
+    </header>
+
+    <div id="main">
+	<?php
+	$days = "365";
+	$curyear = date("Y");
+	$is_leapyear = date('%L');
+	if ($is_leapyear == "1")
+		$days = "366";
+	$day = date("d");				
+	$month = date("m");				
+	$year = date("Y");					
+	if ($cur_year != "")
+		$year = $cur_year;
+	if ($cur_month != "")
+		$month = $cur_month;
+	if ($cur_day != "")
+		$day = $cur_day;
+
+	$longmonth = date("F", mktime(0, 0, 0, $month, $day, $year));
+	$last_year = date("Y", strtotime("$day $longmonth $year -1 day"));
+	$last_month = date("m", strtotime("$day $longmonth $year -1 day"));
+	$last_day = date("d", strtotime("$day $longmonth $year -1 day"));
+	
+	if (isset($_GET['update'])) {
+	?>
+	<div class="date">
+		<?php
+			echo date("j M Y", mktime(0, 0, 0, $month, $day, $year));
+		?>
+	</div>
+	<div class="previous"><a href="/mood/?year=<?=$last_year?>&month=<?=$last_month?>&day=<?=$last_day?>">&laquo;</a></div>
+	<div class="choose">
+		<ul>
+			<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+			<input type="hidden" name="year" value="<?php echo $year ?>">
+			<input type="hidden" name="month" value="<?php echo $month ?>">
+			<input type="hidden" name="day" value="<?php echo $day ?>">
+			<li><input type="image" src="/mood/smileys/1.png" name="mood" value="1" alt="great!"></li>
+			<li><input type="image" src="/mood/smileys/2.png" name="mood" value="2" alt="good"></li>
+			<li><input type="image" src="/mood/smileys/3.png" name="mood" value="3" alt="meh."></li>
+			<li><input type="image" src="/mood/smileys/4.png" name="mood" value="4" alt="bad"></li>
+			<li><input type="image" src="/mood/smileys/5.png" name="mood" value="5" alt="terrible"></li>
+			</form>
+		</ul>
+	</div>
+	<div class="next"><a href="#">&raquo;</a></div>
+	<?php
+	} else {
+		for ($i=1; $i<=12; $i++) {
+			$month_num = date("M", mktime(0,0,0,$i,1,$curyear));
+			$days_in_month = date("t", mktime(0,0,0,$i,1,$curyear));
+			for ($j=1; $j<=$days_in_month; $j++) {
+				$mon = $i;
+				if ($i < 10)
+					$mon = "0".$i;
+				$day = $j;
+				if ($j < 10)
+					$day = "0".$j;
+				$query = 'select mood from mood where year="'.$curyear.'" and month="'.$mon.'" and day="'.$day.'" limit 1';
+		 		$moods = $db->prepare($query);
+				$moods->execute();
+				$fulldate = $curyear.$mon.$day;
+				$png_number = "0";
+				$png_class = " opaque";
+				echo "\n<div class=\"smiley\"><img src=\"/mood/smileys/";
+				while ($mood = $moods->fetchObject()):
+					$mood_int = $mood->mood;
+					if ($mood_int != "") {
+						$png_class = "";
+						$png_number = $mood_int;
+					}
+				endwhile;
+				
+				echo $png_number;
+				if ($j == "1")
+					echo "-01";
+				echo ".png\" class=\"$fulldate $png_class";
+				echo "\"><span class=\"date\">$j</span>";
+				echo "</div>";
+		
+			}
+		}
+	}
+	?>
+    </div>
+	<div style="clear: both"></div>
+    <footer>
+All code &copy; 2010 <a href="mailto:johnke@gmail.com">johnke@gmail.com</a> - based on an idea by <a href="http://brigadacreativa.bigcartel.com/product/life-calendar-how-was-your-day">Brigada Creativa</a>
+    </footer>
+</div> <!--! end of #container -->
+</body>
+</html>
